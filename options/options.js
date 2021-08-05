b3f2a015d5b239e746ade4215be0ef8f329e27a9
Crowdbotics <team@crowdbotics.com>
2021-08-05 00:57:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "falling_shape_29485Identifier",
+  url: "https://falling_shape_29485Identifier.botics.co",
+  api: "https://falling_shape_29485Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
